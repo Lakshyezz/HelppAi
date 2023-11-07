1d3c3528f5b364f544372658b731f67d31b96128
lakshay deep <deep.lakshya1310@gmail.com>
2023-11-08 02:06:30 +0530

    chatengine

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Chat from '@/components/chat'
 
 
+
 function App() {
 
   return (
--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced'
+import Header from "@/components/customHeader"
 
 const Chat = () => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "test-user",
+    "1234"
+  )
+
   return (
-    <div>Chat</div>
+    <div style={{ flexBasis: "100%"}}>
+      <MultiChatSocket {...chatProps}/>
+      <MultiChatWindow {...chatProps}
+        style={{height: "100vh"}}
+        renderChatHeader={(chat) => <Header chat = {chat}/>}
+        // renderMessageForm={(props) => {
+        //   return (<StandardMessageForm/>)
+        //   }}
+      />
+    </div>
   )
 }
 
